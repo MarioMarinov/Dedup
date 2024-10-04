35a7a7230ad40261ad018c7d7aeb3fbaabb91b9e
Mario Marinov <mario.marinov@gmail.com>
2024-10-04 19:39:57 +0300

    Right pane interaction optimisation

---
+++ DedupWinUI/VisibilityStateTrigger.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace DedupWinUI
+{
+    public class VisibilityStateTrigger : StateTriggerBase
+    {
+        private Visibility _visibility;
+        private Visibility _triggerVisibility;
+
+        public Visibility Visibility
+        {
+            get => _visibility;
+            set
+            {
+                _visibility = value;
+                UpdateTrigger();
+            }
+        }
+
+        public Visibility TriggerVisibility
+        {
+            get => _triggerVisibility;
+            set
+            {
+                _triggerVisibility = value;
+                UpdateTrigger();
+            }
+        }
+
+        private void UpdateTrigger()
+        {
+            SetActive(Visibility == TriggerVisibility);
+        }
+    }
+}
